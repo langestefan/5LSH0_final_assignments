99ba313620a7a9ddeda44272c7b017feb977c04f
Stefan <langestefan@msn.com>
2022-01-08 17:42:48 +0100

    Add backpropagation and accuracy measurement
    Signed-off-by: Stefan <langestefan@msn.com>

--- assignment_2_image_retrieval_reid/main.py
+++ assignment_2_image_retrieval_reid/main.py
@@ -2,7 +2,7 @@ from torchvision import datasets
 import torch
 import torchvision
 import torch.nn as nn
-import torch.nn.functional as F
+import torch.nn.functional as func
 import torch.optim as optim
 import numpy as np
 import time
@@ -33,16 +33,67 @@ def import_data(batch_size_train_s, batch_size_test_s):
 
 
 def train():
+    """
+    This function is responsible for training the network's parameters.
+    This code is losely based on the optimization tutorial from PyTorch, see:
+    https://pytorch.org/tutorials/beginner/basics/optimization_tutorial.html
+    """
+    print('--- start training ---')
     network.train()
-    lossf = nn.CrossEntropyLoss()
+    size = len(train_data.dataset)
 
     # loop over all mini-batches
     for batch_id, (mini_batch, label) in enumerate(train_data):
-        optimizer.zero_grad()
-
-        # we input our data N = <batch_size_train> samples at a time
+        # we input our data in N = <batch_size_train> samples at a time
         output = network(mini_batch)
-        print(np.shape(output))
+        # compute loss
+        loss = lossf(output, label)
+        # make sure old gradients are set to 0 before the next step
+        optimizer.zero_grad()
+        # backpropagation
+        loss.backward()
+        optimizer.step()
+
+        if batch_id % 100 == 0:
+            loss, current = loss.item(), batch_id * len(mini_batch)
+            print(f"loss: {loss:>7f}  [{current:>5d}/{size:>5d}]")
+
+
+def test():
+    """
+    Run the trained model on test data.
+    This code is losely based on the optimization tutorial from PyTorch, see:
+    https://pytorch.org/tutorials/beginner/basics/optimization_tutorial.html
+    """
+    print('--- start test ---')
+    size = len(test_data.dataset)
+    num_batches = len(test_data)
+    loss = 0
+    correct_n = np.zeros([10])  # we store correct_n for each digit individually
+    total_n = np.zeros([10])
+
+    # Make sure entire network is enabled and in test mode via .eval()
+    network.eval()
+
+    # with .no_grad() we make sure pytorch does not perform backprogation/gradient calculations
+    with torch.no_grad():
+        for data, labels in test_data:
+            output = network(data)
+            loss += lossf(output, labels).item()
+
+            # check if digit was correctly classified
+            pred_label = torch.max(output, 1)[1].data.squeeze()
+
+            for idx, label in enumerate(labels):
+                total_n[label] += 1
+
+                if pred_label[idx] == label:
+                    correct_n[label] += 1
+
+        # compute correct/total for every digit
+        correct_frac = correct_n / total_n
+
+        print('Fraction correct for each label {}'.format(correct_frac))
 
 
 # network class
@@ -83,46 +134,31 @@ class Network(nn.Module):
             ),
             nn.ReLU(),
             nn.MaxPool2d(kernel_size=7),  # global max of each filter
-
-            # flatten
-            nn.Flatten(),
+            nn.Flatten(),  # flatten to create 32 dimensional input vector for fully connected layer
             nn.Linear(32, 2),  # 2D FC
-            nn.Linear(2, 10)  # 10D FC
-
+            nn.Linear(2, 10),  # 10D FC
         )
-
-        # # 2D-FC
-        # self.fc1 = nn.Linear(32, 2)  # 2D FC
-        # # 10D-FC
-        # self.fc2 = nn.Linear(2, 10)  # 10D FC
 
     def forward(self, x):
         x = self.conv1(x)
         x = self.conv2(x)
         x = self.conv3(x)
-        # # reshape to fit FC layer
-        # x = x.view(x.size(0), -1)
-        # # 32 --> 2 Fully connected layer
-        # x = self.fc1(x)
-        # # 2 --> 10 Fully connected layer
-        # x = self.fc2(x)
-        print('fc2: {0}'.format(np.shape(x)))
         return x
 
 
 if __name__ == '__main__':
     # network settings
-    n_epochs = 1
-    batch_size_train = 64
     batch_size_test = 1000
+
+    # hyperparameters
     LR = 0.01
-    momentum = 0.5
-    n_epochs = 5
+    momentum = 0.9
+    n_epochs = 10
+    batch_size_train = 64
 
     # torch settings
-    seed = 1
-    torch.backends.cudnn.enabled = False
-    torch.manual_seed(seed)
+    torch.backends.cudnn.enabled = False  # turn off GPU processing
+    torch.manual_seed(1)  # to keep random numbers the same over executions
 
     # import data
     train_data, test_data = import_data(batch_size_train, batch_size_test)
@@ -137,8 +173,18 @@ if __name__ == '__main__':
 
     start_time = time.time()
 
+    # nn.CrossEntropyLoss combines nn.LogSoftmax and nn.NLLLoss.
+    lossf = nn.CrossEntropyLoss()
+
+    # test to see performance without any training
+    test()
+
     # train
     for epoch in range(n_epochs):
-        print('~~~~ {} seconds ~~~~'.format(round(time.time() - start_time, 0)))
-        print('Epoch: {}'.format(epoch))
-        print(np.shape(train()))
+        print('--- Epoch: {0} - {1} seconds ---'.format(epoch, round(time.time() - start_time, 0)))
+
+        # 1 training epoch
+        train()
+
+        # test updated model
+        test()
